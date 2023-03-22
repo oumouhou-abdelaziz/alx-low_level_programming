a498298b3d41193467c5d2332363022df868b47a
oumouhou-abdelaziz <Oumouhouabdelaziz@gmail.com>
2023-03-22 08:28:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
 * print_sign - prints the sign of a number
 * @n: the int to check
@@ -10,6 +8,7 @@
 */
 int print_sign(int n)
 {
+		
 if (n > 0)
 {
 _putchar('+');
@@ -23,4 +22,5 @@ return (0);
 _putchar('-');
 }
 return (-1);
+	
 }
