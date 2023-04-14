f2447630e42cacf9bf8350e8f114290b42850d4b
oumouhou-abdelaziz <Oumouhouabdelaziz@gmail.com>
2023-04-14 06:56:46 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,34 +6,34 @@
 		
 /**
 		
- * *malloc_checked - allocates memory using malloc
+* *malloc_checked - allocates memory using malloc
 		
- * @b: number of bytes to allocate
+* @b: number of bytes to allocate
 		
- *
+*
 		
- * Return: a pointer to the allocated memory
+* Return: a pointer to the allocated memory
 		
- */
+*/
 		
 void *malloc_checked(unsigned int b)
 		
 {
 		
-	void *ptr;
+void *ptr;
 		
 
 		
-	ptr = malloc(b);
+ptr = malloc(b);
 		
 
 		
-	if (ptr == NULL)
+if (ptr == NULL)
 		
-		exit(98);
+exit(98);
 		
 
 		
-	return (ptr);
+return (ptr);
 		
 }
